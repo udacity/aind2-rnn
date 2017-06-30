5d381f5d4168d0447ad77d19df18b06e94621c31
scbrubaker02 <scbrubaker02@gmail.com>
2017-06-30 09:22:24 -0400

    Updated to export notebook to html

--- .udacity-pa/projects.py
+++ .udacity-pa/projects.py
@@ -1,17 +1,16 @@
 import argparse
 import shutil
 import os
+import subprocess as sp
 from udacity_pa import udacity
 
 nanodegree = 'nd889'
 projects = ['rnn']
-filenames_all = ['my_answers.py']
+filenames = ['my_answers.py', 'RNN_project.ipynb', 'RNN_project.html']
 
 def submit(args):
-  filenames = []
-  for filename in filenames_all:
-      if os.path.isfile(filename):
-          filenames.append(filename)
+
+  sp.call(['jupyter', 'nbconvert', '--to', 'html', 'RNN_project.ipynb'])
 
   udacity.submit(nanodegree, projects[0], filenames, 
                  environment = args.environment,
