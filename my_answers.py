dd50fe4f9227f6ef96f61cfcee0020f67bc5119e
scbrubaker02 <scbrubaker02@gmail.com>
2017-07-05 09:45:17 -0400

    Fixed assorted argument inconsistencies

--- my_answers.py
+++ my_answers.py
@@ -8,7 +8,7 @@ import keras
 
 # TODO: fill out the function below that transforms the input series 
 # and window-size into a set of input/output pairs for use with our RNN model
-def window_transform_series(series,window_size):
+def window_transform_series(series, window_size):
     # containers for input/output pairs
     X = []
     y = []
@@ -18,30 +18,29 @@ def window_transform_series(series,window_size):
     X.shape = (np.shape(X)[0:2])
     y = np.asarray(y)
     y.shape = (len(y),1)
-    
+
     return X,y
 
 # TODO: build an RNN to perform regression on our time series input/output data
-def build_part1_RNN(step_size, window_size):
+def build_part1_RNN(window_size):
     pass
 
 
 ### TODO: list all unique characters in the text and remove any non-english ones
-def clean_text(text):
+def cleaned_text(text):
     # remove as many non-english characters and character sequences as you can 
     # but don't remove the punctuation given below
     punctuation = ['!', ',', '.', ':', ';', '?']
 
 ### TODO: fill out the function below that transforms the input text and window-size into a set of input/output pairs for use with our RNN model
-def window_transform_text(text,window_size,step_size):
+def window_transform_text(text, window_size, step_size):
     # containers for input/output pairs
     inputs = []
     outputs = []
-    
-  
+
     return inputs,outputs
 
 # TODO build the required RNN model: 
 # a single LSTM hidden layer with softmax activation, categorical_crossentropy loss 
-def build_part2_RNN(step_size, window_size):
+def build_part2_RNN(window_size, num_chars):
     pass
